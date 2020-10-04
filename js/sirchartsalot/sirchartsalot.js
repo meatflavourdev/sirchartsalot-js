cf43285876fc4d907e105a9fcc37b382d876053f
Jeremy Felix D <me@jeremyfelix.com>
2020-10-03 23:00:08 -0700

    Creates sirchartsalot.js

---
+++ js/sirchartsalot/sirchartsalot.js
@@ -0,0 +1,10 @@
+/*
+* -----------------------------------------------------------------------------
+SirChartsALot JS
+* Version: 0.0.1
+* https://www.jeremyfelix.com
+* Copyright, Jeremy Dombrowski
+* Licensed under MIT (https://www.gethalfmoon.com/license)
+* -----------------------------------------------------------------------------
+* The above notice must be included in its entirety when this file is used.
+*/
