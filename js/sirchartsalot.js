009d05beec9c38018635b9a2e300d3d10a30d98c
Jeremy Felix D <me@jeremyfelix.com>
2020-10-14 03:16:57 -0700

    SVG js prototype

--- js/sirchartsalot.js
+++ js/sirchartsalot.js
@@ -8,3 +8,7 @@ SirChartsALot JS
 * -----------------------------------------------------------------------------
 * The above notice must be included in its entirety when this file is used.
 */
+
+var draw = SVG().addTo('#graph-01').size('100%', '500');
+var rect01 = draw.rect(100, 100).attr({ fill: '#f06' });
+var rect02 = draw.rect(100, 100).attr({ fill: '#000' }).move(200, 350);
