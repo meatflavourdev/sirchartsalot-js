fd4b0d6e4df63dec0e8f85ef8faad1ba84f09922
Jeremy Felix D <me@jeremyfelix.com>
2020-10-20 03:21:08 -0700

    Add livereload and serve rollup plugins for better dev experience

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,17 @@
-import cssbundle from 'rollup-plugin-css-bundle';
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import cssbundle from "rollup-plugin-css-bundle";
 
 export default {
-  input: 'src/main.js',
+  input: "src/main.js",
   output: {
-    file: 'dist/sirchartsalot.js',
-    name: 'sirChart',
-    format: 'iife'
+    file: "dist/sirchartsalot.js",
+    name: "sirChart",
+    format: "iife",
   },
-  plugins: [cssbundle()]
+  plugins: [
+    serve(),
+    livereload(),
+    cssbundle()
+  ],
 };
